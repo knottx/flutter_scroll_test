f2ff7d02f3084a16abc042610e48883df77c56de
Knot <knotto.vt@gmail.com>
2025-11-12 22:14:18 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Scroll',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Scroll'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Scrollbar(
+        child: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (context, index) {
+            return ListTile(title: Text('Item $index'));
+          },
+        ),
+      ),
+    );
+  }
+}
